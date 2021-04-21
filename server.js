0fee61fd7bbe2265937cb2f579a489fc3e16c60b
Morgan Bailey <mabailey079@Gmail.com>
2021-04-21 18:18:13 -0400

    updating server for db

--- server.js
+++ server.js
@@ -17,9 +17,9 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb",
  {  useNewUrlParser: true,
-    // useUnifiedTopology: true,
-    // useCreateIndex: true,
-    // useFindAndModify: false 
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false 
   });
 
 // routes
